b260b213b72b102f8af59240129edea6777d7fa9
Krystle Lau <krystlelau90@gmail.com>
2022-04-02 16:16:42 +0800

    added holiday list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Seed from "./pages/Seed";
 import Create from "./pages/Create";
+import HolidayList from "./pages/HolidayList";
 
 function App() {
   return (
     <div className="App">
+      <HolidayList />
       <Create />
     </div>
   );

617d3f9bf39c8775972f7aca6ce6e082fc3a7211
Krystle Lau <krystlelau90@gmail.com>
2022-04-02 12:03:27 +0800

    added use effect

--- src/pages/Seed.jsx
+++ src/pages/Seed.jsx
@@ -1,13 +1,14 @@
-const seed = [
-  {
-    name: "New Year's Day",
-  },
-  {
-    name: "Good Friday",
-  },
-];
+import { useEffect, useState } from "react";
 
 function Seed() {
+  const [seed, setSeed] = useState([]);
+
+  useEffect(() => {
+    fetch("https://krystle-holidays-backend.herokuapp.com/api/holidays/seed")
+      .then((response) => response.json())
+      .then((data) => setSeed(data));
+  }, []);
+
   return (
     <>
       <h1>Seed</h1>

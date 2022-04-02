eb3d924e42e595d12d153dd8d8325c34d383f3e9
Krystle Lau <krystlelau90@gmail.com>
2022-04-02 16:20:26 +0800

    changed holiday list

--- src/pages/HolidayList.jsx
+++ src/pages/HolidayList.jsx
@@ -6,19 +6,26 @@ function HolidayList() {
   const [holidays, setHolidays] = useState([]);
 
   useEffect(() => {
-    fetch(urlcat(BACKEND, "/api/holidays/seed"))
+    fetch(urlcat(BACKEND, "/api/holidays/"))
       .then((response) => response.json())
       .then((data) => setHolidays(data));
   }, []);
+
+  const handleDelete = (id) => () => {
+    const url = urlcat(BACKEND, `/api/holidays/${id}`);
+    fetch(url, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => setHolidays(data));
+  };
 
   return (
     <>
       <ul>
         {holidays.map((holiday) => (
-          <li key={holiday.id}>
+          <li key={holiday._id}>
             <a href="/">{holiday.name}</a>
             {" -- "}
-            <span>Delete</span>
+            <span onClick={handleDelete(holiday._id)}>Delete</span>
           </li>
         ))}
       </ul>
